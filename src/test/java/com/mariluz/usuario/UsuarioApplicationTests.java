46dbd008a0576f37306afb32bafb4b825629984c
Bruno0416 <brunomcalderonv@gmail.com>
2026-06-21 23:00:11 -0400

    fix: propiedades jwt.secret falsa para test contextLoads()

--- src/test/java/com/mariluz/usuario/UsuarioApplicationTests.java
+++ src/test/java/com/mariluz/usuario/UsuarioApplicationTests.java
@@ -3,18 +3,19 @@ package com.mariluz.usuario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-	"spring.datasource.url=jdbc:h2:mem:testdb",
-	"spring.datasource.driverClassName=org.h2.Driver",
-	"spring.datasource.username=sa",
-	"spring.datasource.password=password",
-	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-	"spring.jpa.hibernate.ddl-auto=create-drop"
-})
+@SpringBootTest(
+    properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=password",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "jwt.secret=98PAzqkLUAElxtt+iNCg3GxPvqHI8ytzNfilW2S3RhQ=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+    }
+)
 class UsuarioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
